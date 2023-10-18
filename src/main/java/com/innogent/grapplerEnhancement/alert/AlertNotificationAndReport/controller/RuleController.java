cac3b8ccb5163d02a88e352a0f8d6bbc7cfc981e
Rahul Kumar Sen <93871349+RahulKumar8349@users.noreply.github.com>
2023-10-18 18:49:15 +0530

    swagger work is done

---
+++ src/main/java/com/innogent/grapplerEnhancement/alert/AlertNotificationAndReport/controller/RuleController.java
@@ -0,0 +1,49 @@
+package com.innogent.grapplerEnhancement.alert.AlertNotificationAndReport.controller;
+
+import com.innogent.grapplerEnhancement.alert.AlertNotificationAndReport.entity.Rule;
+import io.swagger.v3.oas.annotations.Operation;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class RuleController {
+
+    @Operation(summary = "Get list of rule", description = "Returns list of rule")
+    @GetMapping("/rule")
+    public ResponseEntity<List<Rule>> getRule(){
+
+        return new ResponseEntity<>(new ArrayList<>(), HttpStatus.OK);
+    }
+
+    @Operation(summary = "Get list of rule by scope", description = "Returns list of rule by scope")
+    @GetMapping("/rule/{scope , identity , trigger , condition}")
+    public ResponseEntity<List<Rule>> getRuleByScope(@PathVariable("scope")String scope,@PathVariable("identity")String identity,@PathVariable("trigger")String trigger,@PathVariable("scope")String condition){
+
+        return new ResponseEntity<>(new ArrayList<>(), HttpStatus.OK);
+    }
+
+    @Operation(summary = "create a rule", description = "Returns created rule")
+    @PostMapping("/rule")
+  public ResponseEntity<Rule> createRule(@RequestBody Rule rule){
+        return ResponseEntity.ok(new Rule());
+    }
+
+    @Operation(summary = "Soft Delete a rule", description = "Returns soft deleted status")
+    @DeleteMapping("/rule/{ruleId}")
+    public ResponseEntity<Rule> deleteRule(@PathVariable("ruleId")Long ruleId){
+        return ResponseEntity.ok(new Rule());
+    }
+
+    @Operation(summary = "update rule", description = "Returns the updated rule")
+    @PutMapping("/rule/{ruleId}")
+    public ResponseEntity<Rule> updateRule(@PathVariable("ruleId")Long ruleId){
+        return ResponseEntity.ok(new Rule());
+    }
+
+
+
+}
--- src/main/java/com/innogent/grapplerEnhancement/alert/AlertNotificationAndReport/controller/TicketController.java
+++ src/main/java/com/innogent/grapplerEnhancement/alert/AlertNotificationAndReport/controller/TicketController.java
@@ -1,19 +1,20 @@
 package com.innogent.grapplerEnhancement.alert.AlertNotificationAndReport.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api")
+//@RequestMapping("/api")
 public class TicketController {
 
     @Operation(summary = "Get a Ticket by TicketId", description = "Returns a Ticket as per the TicketId")
     @GetMapping("/ticket/{ticketId}")
-    public String getTicket(@PathVariable("ticketId")Long ticketId){
-        return "this is your ticket of id : "+ticketId;
+    public ResponseEntity<String> getTicket(@PathVariable("ticketId")Long ticketId){
+        return ResponseEntity.ok("this is your ticket of id : "+ticketId);
     }
 
 //    @Operation(summary = "Daily Update", description = "Returns the daily Update")
